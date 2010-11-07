bb34fed042a8315d727b106a2c9ef0df16b3ab0c
Alexander Werner <bundeswerner@googlemail.com>
2010-11-07 20:28:58 +0100

    Added License Header

--- src/pymime.py
+++ src/pymime.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#    Copyright 2010 Alexander Werner
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 This script takes a MIME-formatted email and does various transformations with it,
 e.g. converts HTML-mails to plain text mails and strips attachements.
