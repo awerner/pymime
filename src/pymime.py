263a24898ce945199e24ea0c9618b87b5d2392ae
Alexander Werner <alex@documentfoundation.org>
2011-03-13 01:24:02 +0100

    Updated copyright line

--- src/pymime.py
+++ src/pymime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#    Copyright 2010 Alexander Werner
+#    Copyright 2011 Alexander Werner
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
