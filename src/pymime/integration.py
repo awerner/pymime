06aa057f786f8fe382e2d36eaaa1cdd76c65f370
Alexander Werner <bundeswerner@googlemail.com>
2011-05-02 17:37:52 +0200

    Added flat_store store-function

--- src/pymime/integration.py
+++ src/pymime/integration.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 #    Copyright 2011 Alexander Werner
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -14,5 +13,57 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import mimetypes
+import hashlib
+import os
+
 def dummy_store(attachments, options):
+    """
+    Simple store-function that returns a text with the number of attachments
+    and does nothing with them, so they are lost.
+    """
     return "Number of Attachments: {0}".format(len(attachments))
+
+def flat_store(attachments,options):
+    """
+    This store-function saves the detached attachments in the directory options["path"]
+    (default: current working dir + store).
+    Subdirectories containing the attachments are created named after the sha1-hash of the attachment, salted with options["salt"].
+    If specified, options["text_after"] is appended to the attachment list.
+    """
+    salt = ""
+    if hasattr(options,"salt"):
+        salt = options["salt"]
+    path = os.path.join(os.getcwd(),"store")
+    if hasattr(options,"path"):
+        path=options["path"]
+    baseurl="http://localhost/"
+    if hasattr(options,"baseurl"):
+        baseurl=options["baseurl"]
+    text_after=""
+    if hasattr(options, "text_after"):
+        text_after=options["text_after"]
+    footer="Number of allowed Attachments: {0}".format(len(attachments))
+    for part in attachments:
+        filename = part.get_filename()
+        if not filename:
+            ext = mimetypes.guess_extension(part.get_content_type())
+            if not ext:
+                ext = '.bin'
+            filename = "{0}{1}".format("attachment",ext)
+        h = hashlib.sha1(salt) #@UndefinedVariable
+        h.update(part.get_payload(decode=True))
+        hex = h.hexdigest()
+        dir = os.path.join(path,hex)
+        filepath=os.path.join(dir,filename)
+        try:
+            os.makedirs(dir)
+        except os.error:
+            pass
+        fp = open(filepath, 'wb')
+        fp.write(part.get_payload(decode=True))
+        fp.close()
+        fullurl="{0}{1}/{2}".format(baseurl,hex,filename)
+        footer+="\nAttachment: {0}".format(fullurl)
+    footer+=text_after
+    return footer
