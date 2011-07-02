e97b86a5deeebeade5fdcaf6736b9732bbf76c35
Alexander Werner <alex@documentfoundation.org>
2011-07-02 22:22:56 +0200

    Added wsgi handler, updated .gitignore and minor change to urls.py

---
+++ integration/django_attachmentstore/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django_attachmentstore.views.home', name='home'),
+    url(r'^', include('pymime.django_app.pymime_attachmentservice.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+) + static("/media/", document_root = settings.MEDIA_ROOT)
