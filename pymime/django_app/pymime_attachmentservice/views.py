4694f08245fd391fbeae89d69446c78013731c1e
Alexander Werner <alex@documentfoundation.org>
2011-10-15 16:31:00 +0200

    Mailing managers on problem report

--- pymime/django_app/pymime_attachmentservice/views.py
+++ pymime/django_app/pymime_attachmentservice/views.py
@@ -20,6 +20,14 @@ def report_mail(request, uuid):
             r.headers = str(request.META)
             r.reporter_address = reporter_mail
             r.save()
+            from django.core.mail import mail_managers
+            mail_managers("Attachment reported",
+                          "A suspicious mail was reported." \
+                          "Please check the following report and act accordingly:\n" \
+                          "Reporter: {reporter_mail}\n" \
+                          "Reporter IP: {reporter_ip}\n" \
+                          "Reason: {reason}\n" \
+                          "Reported Mail: {url}".format(reporter_mail=reporter_mail, reporter_ip=r.ip, reason=reason, url=m.get_admin_url()))
             return render_to_response("show_mail.html", RequestContext(request, {"mail":m}))
     else:
         form = ReportForm()
